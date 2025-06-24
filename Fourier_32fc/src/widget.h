3cf3e881a69acf273f3412ce3b78f87c74930e02
Maxmaxgggg <82994580+Maxmaxgggg@users.noreply.github.com>
2025-06-24 12:33:12 +0300

    Добавил интерполяцию Фурье для fc32 сигналов

---
+++ Fourier_32fc/src/widget.h
@@ -0,0 +1,57 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTimer>
+#include <complex>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_extraSettingsCHB_stateChanged(int arg1);
+
+    void on_inFilePBN_clicked();
+
+    void on_mainPBN_clicked();
+
+    void on_outFilePBN_clicked();
+
+    void on_outFileCancel_clicked();
+
+    void on_overlapSPB_valueChanged(int arg1);
+
+    void on_blockSizeHSL_valueChanged(int value);
+
+    void on_blockSizeCMB_currentTextChanged(const QString &arg1);
+
+private:
+    Ui::Widget *ui;
+
+    float   *inBufMono;
+    double  *inBufMonoDouble;
+    double  *DCT;
+
+    float   *inBufStereo;
+    double  *inBufLeft;
+    double  *inBufRight;
+    float   *outBufLeft;
+    double  *DCT_Left;
+    float   *outBufRight;
+    double  *DCT_Right;
+    QTimer  *timer;
+    QString inFileName;
+    QString outFileName;
+};
+#endif // WIDGET_H
